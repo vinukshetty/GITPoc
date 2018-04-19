345fb546f76630c3536d836f2739751eac4fa03b
vinoth.k <vinu.shetty369@gmail.com>
2018-04-19 18:25:25 +0530

    abc

--- src/main/java/com/mkyong/web/controller/HelloController.java
+++ src/main/java/com/mkyong/web/controller/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 
-		model.addAttribute("message", "Spring 3 MVC Hello World");
+		model.addAttribute("message", "Spring 3 MVC Hello Worldss");
 		return "hello";
 
 	}
